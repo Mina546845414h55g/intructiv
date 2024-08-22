692979c04e807bba068e90ff3dfe6b5063d05797
Mina546845414h55g <hanymina682@gmail.com>
2024-08-22 17:19:47 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+let btn1 = document.getElementById('active1');
+let btn2 = document.getElementById('active2');
+let btn3 = document.getElementById('active3');
+let btn4 = document.getElementById('active4');
+let btn5 = document.getElementById('active5');
+
+
+
+btn1.addEventListener('click',function(){
+     btn1.style.backgroundColor = 'hsl(0, 0%, 100%)'
+     btn1.style.color = 'black'
+     btn2.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn2.style.color = 'hsl(0, 0%, 100%)'
+     btn3.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn3.style.color = 'hsl(0, 0%, 100%)'
+     btn4.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn4.style.color = 'hsl(0, 0%, 100%)'
+     btn5.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn5.style.color = 'hsl(0, 0%, 100%)'
+
+})
+
+btn2.addEventListener('click',function(){
+     btn2.style.backgroundColor = 'hsl(0, 0%, 100%)'
+     btn2.style.color = 'black'
+     btn1.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn1.style.color = 'hsl(0, 0%, 100%)'
+     btn3.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn3.style.color = 'hsl(0, 0%, 100%)'
+     btn4.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn4.style.color = 'hsl(0, 0%, 100%)'
+     btn5.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn5.style.color = 'hsl(0, 0%, 100%)'
+
+})
+
+btn3.addEventListener('click',function(){
+     btn3.style.backgroundColor = 'hsl(0, 0%, 100%)'
+     btn3.style.color = 'black'
+     btn4.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn4.style.color = 'hsl(0, 0%, 100%)'
+     btn5.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn5.style.color = 'hsl(0, 0%, 100%)'
+     btn1.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn1.style.color = 'hsl(0, 0%, 100%)'
+     btn2.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn2.style.color = 'hsl(0, 0%, 100%)'
+})
+
+btn4.addEventListener('click',function(){
+
+     btn4.style.backgroundColor = 'hsl(0, 0%, 100%)'
+     btn4.style.color = 'black'
+     btn5.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn5.style.color = 'hsl(0, 0%, 100%)'
+     btn1.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn1.style.color = 'hsl(0, 0%, 100%)'
+     btn2.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn2.style.color = 'hsl(0, 0%, 100%)'
+     btn3.style.backgroundColor = 'hsl(213, 19%, 18%)'
+     btn3.style.color = 'hsl(0, 0%, 100%)'
+
+})
+
+btn5.addEventListener('click',function(){
+
+    btn5.style.backgroundColor = 'hsl(0, 0%, 100%)'
+    btn5.style.color = 'black'
+    btn1.style.backgroundColor = 'hsl(213, 19%, 18%)'
+    btn1.style.color = 'hsl(0, 0%, 100%)'
+    btn3.style.backgroundColor = 'hsl(213, 19%, 18%)'
+    btn3.style.color = 'hsl(0, 0%, 100%)'
+    btn4.style.backgroundColor = 'hsl(213, 19%, 18%)'
+    btn4.style.color = 'hsl(0, 0%, 100%)'
+    btn2.style.backgroundColor = 'hsl(213, 19%, 18%)'
+    btn2.style.color = 'hsl(0, 0%, 100%)'
+
+})
+
